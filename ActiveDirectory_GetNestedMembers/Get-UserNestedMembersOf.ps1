283ec722aa753df946f8ba38f1390e9668349e6b
PowerMav <lars.hedberg74@gmail.com>
2019-08-07 16:35:02 +0200

    Fixed error with command executing

--- ActiveDirectory_GetNestedMembers/Get-UserNestedMembersOf.ps1
+++ ActiveDirectory_GetNestedMembers/Get-UserNestedMembersOf.ps1
@@ -3,16 +3,16 @@
 Get all groups that a user is member of
 
 .DESCRIPTION
-Long description
+Long description....
 
 .PARAMETER DistinguishedName
-Parameter description
+DN of the User for who you want to list all Groups (like gpresult /r on local computer)
 
 .PARAMETER Groups
-Parameter description
+Used to perform recursive MemberOf (Only used Internally in the Function)
 
 .EXAMPLE
- Get-ADUserNestedGroups -DistinguishedName (Get-ADUser -Identity $User).DistinguishedName
+ Get-ADUserNestedGroups -DistinguishedName (Get-ADUser -Identity ballin).DistinguishedName | Select-Object Name
  
 .NOTES
 Thanks to http://blog.tofte-it.dk/powershell-get-all-nested-groups-for-a-user-in-active-directory/ that did all the work, I just made it faster and Adv Func
@@ -56,5 +56,3 @@ Function Get-ADUserNestedGroups {
     # Return groups.
     Return $Groups
 }
-
-Get-ADUserNestedGroups -DistinguishedName (Get-ADUser -Identity $User).DistinguishedName
